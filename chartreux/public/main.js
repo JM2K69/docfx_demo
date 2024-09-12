60c3739cdc10588f8eb5724619d09f1e585b9e41
JM2K69 <bezettorres.jerome@neuf.fr>
2024-09-12 12:15:40 +0200

    test site

---
+++ chartreux/public/main.js
@@ -0,0 +1,10 @@
+export default {
+    defaultTheme: 'dark',
+    iconLinks: [
+      {
+        icon: 'bar-chart-line-fill',
+        href: 'https://cr-moodle.leschartreux.com/',
+        title: 'Moodle Chartreux'
+      }
+    ]
+  }
